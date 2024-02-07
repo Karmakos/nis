9a227a300381f0c6230674516ea6bfc215e4db33
karmakos <karimakos.murigi@gmail.com>
2024-02-08 02:28:18 +0300

    Change publish dev .toml

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import ejs from 'ejs'
+import path from 'path'; 
+import { fileURLToPath } from 'url';
 
 const app = express();
 const port = 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
 
 app.set('view engine', 'ejs');
 
