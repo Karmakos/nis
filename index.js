e59b14756e57c53eea69fce6e4f76f13a174434a
karmakos <karimakos.murigi@gmail.com>
2024-02-09 01:48:08 +0300

    Turning to Vercel

--- index.js
+++ index.js
@@ -1,91 +1,63 @@
 import express from 'express';
 import ejs from 'ejs'
-import path from 'path'; 
-import { fileURLToPath } from 'url';
-const router = express.Router();
 
 const app = express();
 const port = 3000;
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+
 
 app.use(express.static("public"));
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'views'));
 
 app.set('view engine', 'ejs');
 
-app.use((err, req, res, next) => {
-	console.error(err.stack);
-	res.status(500).send('Something went wrong!');
-  });
-  
-
-router.get('/', (req, res)=>
+app.get('/', (req, res)=>
 	{
 		res.render("index.ejs");
 	});
-router.get('/about-us', (req, res)=>
+app.get('/about-us', (req, res)=>
 {
 	res.render("about-us.ejs")
 
 });
-router.get('/faq', (req, res)=>
+app.get('/faq', (req, res)=>
 {
 	res.render("faq.ejs")
 
 });
-router.get('/resources', (req, res)=>
+app.get('/resources', (req, res)=>
 {
 	res.render("resources.ejs")
 
 });
-router.get('/careers', (req, res)=>
+app.get('/careers', (req, res)=>
 {
 	res.render("careers.ejs")
 
 });
-router.get('/tenders', (req, res)=>
+app.get('/tenders', (req, res)=>
 {
 	res.render("tenders.ejs")
 
 });
-router.get('/tips', (req, res)=>
+app.get('/tips', (req, res)=>
 {
 	res.render("tips.ejs")
 
 });
-router.get('/contact', (req, res)=>
+app.get('/contact', (req, res)=>
 {
 	res.render("contact.ejs")
 
 });
-router.get('/privacy-policy', (req, res)=>
+app.get('/privacy-policy', (req, res)=>
 {
 	res.render("privacy-policy.ejs")
 
 });
-router.get('/tou', (req, res)=>
+app.get('/tou', (req, res)=>
 {
 	res.render("tou.ejs")
 
 });
-export const handler = async (event, context) => {
-	const path = event.path.toLowerCase();
-  
-	const response = {
-	  statusCode: 404,
-	  body: JSON.stringify({ message: 'Not Found' }),
-	};
-  
-	// Define your serverless routes here
-	if (path === 'index') {
-	  response.statusCode = 200;
-	  response.body = JSON.stringify({ message: 'Hello from the serverless function!' });
-	}
-  
-	return response;
-  };
-app.use('/', router);
 app.listen(port,
-console.log(`App running at port ${port}`));
+console.log(`App running at port ${port}`)
+	);
